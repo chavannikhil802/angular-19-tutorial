e8045054c4eef189cfb270e0c567c326a67608e3
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2026-05-19 20:43:48 +0530

    Adding code in ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
     LoginComponent, 
-    SignupComponent
+    SignupComponent, 
+    ProfileComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-profile',
+    template: '<h1>Profile Component</h1>',
+    styles: 'h1 { color: orange }'
+})
+
+export class ProfileComponent {}

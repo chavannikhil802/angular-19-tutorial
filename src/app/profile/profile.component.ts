e763eb278a814ff14d97015a5ee1f3b828d95d6c
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2026-05-19 20:51:26 +0530

    Adding styleUrl to ts file

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,7 +4,8 @@ import { Component } from "@angular/core";
     selector: 'app-profile',
     template: '<h1>Profile Component</h1>',
     styles: 'h1 { color: orange }',
-    templateUrl: './profile.component.html'
+    templateUrl: './profile.component.html',
+    styleUrl: './profile.component.css'
 })
 
 export class ProfileComponent {}

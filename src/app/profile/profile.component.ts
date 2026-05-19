191014b44dd887ab6516cb885491f444d2616db3
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2026-05-19 20:48:02 +0530

    Adding template URL in the ts file

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-profile',
     template: '<h1>Profile Component</h1>',
-    styles: 'h1 { color: orange }'
+    styles: 'h1 { color: orange }',
+    templateUrl: './profile.component.html'
 })
 
 export class ProfileComponent {}
